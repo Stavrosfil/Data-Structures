7e676107d717b0b9b3fa95d53e4c149f84040afa
Stavrosfil <stavrosfil@gmail.com>
2019-02-03 20:39:44 +0200

    Fix initialization for binary search tree as default.
    -Added getters
    -Commented out addChild method

--- Node.java
+++ Node.java
@@ -1,19 +1,36 @@
 import java.util.ArrayList;
 
 public class Node {
-    Node parent = null;
-    ArrayList<Node> children;
-    Object value;
+    private Node parent = null;
+    private ArrayList<Node> children;
+    private int value;
 
-    public Node(Node parent, Object value) {
+    public Node(Node parent, int value) {
         this.parent = parent;
         this.value = value;
-        this.children = new ArrayList<Node>();
-        if (parent != null)
-            this.parent.addChild(this);
+        this.children = new ArrayList<Node>(0);
+
+        // TODO: better initialization method?
+        this.children.add(null);
+        this.children.add(null);
     }
 
-    public void addChild(Node child) {
-        children.add(child);
+    // TODO: Implement addChild method correctly
+    // public void addChild(Node child) {
+    //     children.add(child);
+    // }
+
+    // -------------------GETTERS-------------------
+    
+    public Node getParent() {
+        return this.parent;
+    }
+
+    public ArrayList<Node> getChildren() {
+        return this.children;
+    }
+
+    public int getValue() {
+        return this.value;
     }
 }
